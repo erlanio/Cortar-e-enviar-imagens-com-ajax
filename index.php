9896ca1cf8d5dbd81d84cf5e0499043b6422262f
erlanio.freire <erlanio.desenvolvedor@gmail.com>
2019-04-03 22:32:36 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<html>  
+    <head>  
+        <title>Upload de Imagem AJAX</title>  
+		
+		<script src="jquery.min.js"></script>  
+		<script src="bootstrap.min.js"></script>
+		<script src="croppie.js"></script>
+		<link rel="stylesheet" href="bootstrap.min.css" />
+		<link rel="stylesheet" href="croppie.css" />
+    </head>  
+    <body>  
+        <div class="container">
+          <br />
+      <h3 align="center">Cortar Imagem Ajax</h3>
+      <br />
+      <br />
+			<div class="panel panel-default">
+  				<div class="panel-heading">Selecione uma imagem</div>
+  				<div class="panel-body" align="center">
+  					<input type="file" name="upload_image" id="upload_image" />
+  					<br />
+  					<div id="uploaded_image"></div>
+  				</div>
+  			</div>
+  		</div>
+    </body>  
+</html>
+
+<div id="uploadimageModal" class="modal" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+      		<div class="modal-header">
+        		<button type="button" class="close" data-dismiss="modal">&times;</button>
+        		<h4 class="modal-title">Upload & Cortar Imagem</h4>
+      		</div>
+      		<div class="modal-body">
+        		<div class="row">
+  					<div class="col-md-8 text-center">
+						  <div id="image_demo" style="width:350px; margin-top:30px"></div>
+  					</div>
+  					<div class="col-md-4" style="padding-top:30px;">
+  						<br />
+  						<br />
+  						<br/>
+						  <button class="btn btn-success crop_image">Upload & Cortar Imagem</button>
+					</div>
+				</div>
+      		</div>
+      		<div class="modal-footer">
+        		<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+      		</div>
+    	</div>
+    </div>
+</div>
+
+<script>  
+$(document).ready(function(){
+
+	$image_crop = $('#image_demo').croppie({
+    enableExif: true,
+    viewport: {
+      width:200,
+      height:200,
+      type:'square' //circle
+    },
+    boundary:{
+      width:300,
+      height:300
+    }
+  });
+
+  $('#upload_image').on('change', function(){
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      $image_crop.croppie('bind', {
+        url: event.target.result
+      }).then(function(){
+        console.log('jQuery bind complete');
+      });
+    }
+    reader.readAsDataURL(this.files[0]);
+    $('#uploadimageModal').modal('show');
+  });
+
+  $('.crop_image').click(function(event){
+    $image_crop.croppie('result', {
+      type: 'canvas',
+      size: 'viewport'
+    }).then(function(response){
+      $.ajax({
+        url:"upload.php",
+        type: "POST",
+        data:{"image": response},
+        success:function(data)
+        {
+          $('#uploadimageModal').modal('hide');
+          $('#uploaded_image').html(data);
+        }
+      });
+    })
+  });
+
+});  
+</script>
